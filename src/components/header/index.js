6bae35a1e05e34fa0f39203338cf2b33310a6cc8
philomenambura <philomenambura@gmail.com>
2020-04-19 15:28:15 +0300

    edit

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,7 +4,7 @@ import style from './style';
 
 const Header = () => (
 	<header class={style.header}>
-		<Link href="/"><h1>The android Guide</h1></Link>
+		<Link href="/"><h1>The Android Guide</h1></Link>
 		<nav>
 			<Link activeClassName={style.active} href="/blogs">Blogs</Link>
 			<Link activeClassName={style.active} href="/contact">Contact me</Link>
